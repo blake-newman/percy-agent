34bde7a2e0988a4b4e6f864df0e388a7e6e2e7f6
Blake Newman <blakenoodles@gmail.com>
2019-05-27 17:56:01 +0100

    destroy instead

--- dist/services/asset-discovery-service.js
+++ dist/services/asset-discovery-service.js
@@ -178,7 +178,7 @@ class AssetDiscoveryService extends percy_client_service_1.default {
             const maybeResources = await Promise.all(maybeResourcePromises);
             logger_1.profile('--> assetDiscoveryServer.waitForResourceProcessing');
             logger_1.profile('--> assetDiscoveryService.pool.release', { url: rootResourceUrl });
-            await pool.release(page);
+            await pool.destroy(page);
             logger_1.profile('--> assetDiscoveryService.pool.release');
             return maybeResources.filter((maybeResource) => maybeResource != null);
         }
