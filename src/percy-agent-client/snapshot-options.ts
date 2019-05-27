d7b329accf639f1abc8ab8fb9efeaf4a4bb634a6
Blake Newman <blakenoodles@gmail.com>
2019-05-27 17:45:28 +0100

    remove src

--- src/percy-agent-client/snapshot-options.ts
+++
@@ -1,8 +0,0 @@
-export interface SnapshotOptions {
-  enableJavaScript?: boolean,
-  enableJavascript?: boolean, // deprecated. Use enableJavaScript
-  widths?: number[],
-  minHeight?: number,
-  minimumHeight?: number, // deprecated. Use minHeight
-  document?: Document,
-}
