dfc67dcb5c269627df8a816cc4a4b560da231835
Blake Newman <blakenoodles@gmail.com>
2019-12-06 15:48:40 +0000

    [release]

--- src/configuration/agent-configuration.ts
+++
@@ -1,6 +0,0 @@
-import { AssetDiscoveryConfiguration } from './asset-discovery-configuration'
-
-export interface AgentConfiguration {
-  'port': number,
-  'asset-discovery': AssetDiscoveryConfiguration
-}
